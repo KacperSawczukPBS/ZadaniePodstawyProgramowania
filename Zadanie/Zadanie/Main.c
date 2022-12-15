5bc38e48eed4dd147649d57aded94dc410cc1cf1
Kacper Sawczuk <sawczuk.k@tlen.pl>
2022-12-15 20:01:21 +0100

    5ty commit

--- Zadanie/Zadanie/Main.c
+++ Zadanie/Zadanie/Main.c
@@ -3,6 +3,17 @@
 #include "Main.h"
 #define SIZE 20
 
+void Shorten(char* string, int number) {
+	if (strlen(string) < number) {
+		printf("\nNie mozesz tego zrobic");
+		return;
+	}
+
+	string[number] = "\0";
+
+	fputs(string, stdout);
+}
+
 int main() {
 	char string[SIZE];
 
