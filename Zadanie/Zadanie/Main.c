b627c5e6b14d4729ae3621ebc903efb43e1ab7f9
Kacper Sawczuk <sawczuk.k@tlen.pl>
2022-12-15 20:00:02 +0100

    4ty commit

--- Zadanie/Zadanie/Main.c
+++ Zadanie/Zadanie/Main.c
@@ -1,7 +1,20 @@
 #include <string.h>
 #include <stdio.h>
+#include "Main.h"
 #define SIZE 20
 
 int main() {
+	char string[SIZE];
+
+	fgets(string, SIZE, stdin);
+	fputs(string, stdout);
+
+	printf("\nPodaj liczbe:");
+
+	int number;
+
+	scanf_s("%d", &number);
+
+	Shorten(string, number);
 
 }
